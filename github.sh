69f0c605590c89428c78ec25cddff8f277d028a9
AndresQuinto5 <qui18288@uvg.edu.gt>
2023-09-11 12:46:40 -0600

    [FIX]

--- github.sh
+++ github.sh
@@ -1,3 +1,21 @@
-#! /bin/sh
+#!/bin/sh
 
-git add * ; git commit -m $1
+if [ -z "$1" ]; then
+  echo "Error: No commit message provided."
+  exit 1
+fi
+
+git add *
+git commit -m "$1"
+
+# Preguntar al usuario si desea hacer un push
+read -p "¿Deseas hacer un push al repositorio remoto? (s/n): " respuesta
+
+case $respuesta in
+  [sS]|[yY])
+    git push origin master
+    ;;
+  *)
+    echo "Operación de push cancelada."
+    ;;
+esac
